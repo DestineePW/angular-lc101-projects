fb84d2f76051ea6c4f26db5e321d8cdff95c371c
DestineePW <95252380+DestineePW@users.noreply.github.com>
2022-12-13 22:00:57 -0600

    exercise part one finished

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Movie Promo Posters';
+  image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0hS4CSKWq4L0GxVDkv0-6gRoSiXEwwEMQ9_NNGHAc&s';
+  image2 = 'https://ih1.redbubble.net/image.505815630.9008/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg';
+  image3 = 'https://m.media-amazon.com/images/M/MV5BZGRkOGMxYTUtZTBhYS00NzI3LWEzMDQtOWRhMmNjNjJjMzM4XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg';
 
   constructor() { }
 
